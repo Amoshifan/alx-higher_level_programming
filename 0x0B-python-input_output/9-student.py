b24b56936fca6f07199b5eabfcb46c94225da0fc
Amos Aondover Kwaghfan <callamos88@gmail.com>
2023-04-17 23:26:22 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines "Student" class
+"""
+
+
+class Student:
+    """Student """
+    def __init__(self, first_name, last_name, age):
+        """Initializes student's information"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance """
+        return self.__dict__
