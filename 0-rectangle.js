f526c9b4848b73b3c3ee2338343d7adb90e31d01
Amos Aondover Kwaghfan <callamos88@gmail.com>
2023-06-15 21:58:45 -0700

    JavaScript Objects

---
+++ 0-rectangle.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// empty class that defines a rectangle
+module.exports = class Rectangle {
+
+};
---
+++ 1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* You must use the class notation for defining your class
+The constructor must take 2 arguments w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
